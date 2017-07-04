2344120c392107c215c4acd8a8b3e89b136c3594
CodeHedgehog <codehedgehog@outlook.com>
2017-07-04 13:46:31 -0700

    Controller methods and views https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/controller-methods-views

--- AspNetCoreMvcMovie/Models/Movie.cs
+++ AspNetCoreMvcMovie/Models/Movie.cs
@@ -8,7 +8,9 @@
 		public int ID { get; set; }
 		public string Title { get; set; }
 
+		[Display(Name = "Release Date")]
 		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		[DataType(DataType.Date)]
 		public DateTime ReleaseDate { get; set; }
 
 		public string Genre { get; set; }
